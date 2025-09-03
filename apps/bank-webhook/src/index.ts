dc3270a8fdd0f44c501ba8ee15e5c91ed8917357
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2025-09-03 23:36:24 +0530

    package.json changed in web-hook

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,41 +1,55 @@
-import express from "express"
-import db from "@repo/db/client"
+import express from "express";
+import db from "@repo/db/client";
 const app = express();
 
-app.post("/hdfcwebhook", async(req, res) => {
-  const paymentInformation  = {
-    token: req.body.token,
-    userId : req.body.user_identifier,
-    amount: req.body.amount,
-  }
+app.use(express.json())
 
-await db.balance.update({
-    where:{
-        userId : paymentInformation.userId
-    },
-    data : {
-        amount:{
-            increment: paymentInformation.amount
-        }
+app.post("/hdfcWebhook", async (req, res) => {
+    //TODO: Add zod validation here?
+    //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+    const paymentInformation: {
+        token: string;
+        userId: string;
+        amount: string
+    } = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    };
+
+    try {
+        await db.$transaction([
+            db.balance.updateMany({
+                where: {
+                    userId: Number(paymentInformation.userId)
+                },
+                data: {
+                    amount: {
+                        // You can also get this from your DB
+                        increment: Number(paymentInformation.amount)
+                    }
+                }
+            }),
+            db.onRampTransaction.updateMany({
+                where: {
+                    token: paymentInformation.token
+                }, 
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+
+        res.json({
+            message: "Captured"
+        })
+    } catch(e) {
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
     }
+
 })
 
-await db.onRampTransaction.update({
-    where:{
-        token: paymentInformation.token
-    },
-    data:{
-        status: "SUCCESS"
-    }
-})
-
-res.status(200).json({ 
-    message: "captured"
-});
-
-});
-
-
-app.listen(4000, () => {
-  console.log("Server is running on http://localhost:4000");
-});
+app.listen(3003);
