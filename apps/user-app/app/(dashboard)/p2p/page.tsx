b1c59eed190ef6f22585421e69195c0138a18b40
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2025-09-08 22:52:14 +0530

    added P2P

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,9 @@
+import { SendCard } from "../../../components/SendCard";
+
+export default function(){
+    return(
+        <div className="max-w-2xl mx-auto mt-8">
+            <SendCard />
+        </div>
+    )
+}
