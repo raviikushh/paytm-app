a32bc84bb7b48f8a83bd2ded048114fed9b2c26e
Ravi Kumar kushwaha <ravikushwaha2031@gmail.com>
2025-09-09 23:24:25 +0530

    changes in p2p transfer

--- apps/user-app/app/lib/actions/p2pTranafer.ts
+++ apps/user-app/app/lib/actions/p2pTranafer.ts
@@ -40,5 +40,13 @@ export async function p2pTransfer(to: string, amount: number) {
             where: { userId: toUser.id },
             data: { amount: { increment: amount } },
           });
+          await tx.p2pTransfer.create({
+            data: {
+                fromUserId: Number(from),
+                toUserId: toUser.id,
+                amount: amount,
+                timestamp: new Date()
+            }
+          })
     });
 }
